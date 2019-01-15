c81837edcf492e63b7b5f58410badccdcb768280
Reza Motlagh <rmoti49@gmail.com>
2019-01-15 07:41:51 +0300

    Random String Function Added.

--- system/commons.php
+++ system/commons.php
@@ -59,6 +59,16 @@ function getBaseUrl()
     return Config::SITE['base_url'];
 }
 
+function randomStr($length = 16)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 function output($status,$data)
 {
